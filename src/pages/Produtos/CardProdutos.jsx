daf0a6aeb3b5e8d05ad92a5869f40d6db2aa8fcf
Lorrany de Oliveira <116725554+Lorrany2004@users.noreply.github.com>
2023-03-30 12:21:23 -0300

    Update CardProdutos.jsx

--- src/pages/Produtos/CardProdutos.jsx
+++ src/pages/Produtos/CardProdutos.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import "./CardProdutos.css"
 
 const CardProdutos = ({ nome, cor, preco, img}) => {
-  return (
-    <div className='container'>
+    
+return(
+        <div className='container'>
+        <img src={img}/>
         <h2>Produto</h2>
         <p>Nome: {nome}</p>
         <p>Cor: {cor}</p>
         <p>Preco: {preco}</p>
-        <img src={img}/>
-    </div>
-  )
-}
+        </div>
+)}
 
 export default CardProdutos
